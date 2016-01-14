1a3c034237922cb5af76a34db5960fa998a05083
Saul Ocampo <digitalh2o@localhost.localdomain>
2016-01-14 00:04:59 -0500

    Made changes, work in progress

--- tictac.rb
+++ tictac.rb
@@ -3,52 +3,67 @@ class Game
 	attr_accessor :player1, :player2, :board
 
 	def initialize
+		@board = Board.new
 		@player1 = Player.new("Player 1", "X")
 		@player2 = Player.new("Player 2", "O")
-		@board = Board.new
 	end
 
 	#these are the winning combinations that will end the game
-	#$winning_combinations = [[012],[048],[036],[147],[258],[246],[345],[678]]
+	def win
+		$winning_combinations = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
+	end
 
 	#this gathers the information of the players and uses the Player class to add the information
 	def start
 		puts "Let's play a game of Tic Tac Toe!"
-		puts "#{@player1.name}, your symbol will be #{@player1.symbol}"
-		puts "#{@player2.name}, your symbol will be #{@player2.symbol}"	
-		show_board
+		puts "What is your name Player 1?"
+		name = gets.chomp
+		puts "Welcome #{name}! You will be #{@player1.name} and your symbol will be #{@player1.symbol}!"
+		puts "What is your name Player 2?"
+		second_name = gets.chomp
+		puts "Welcome #{second_name}! You will be #{@player2.name} and your symbol will be #{@player2.symbol}!"	
+		board.show_board
 	end
+
+	
+
+	#this will allow the player to pick a location
+	def make_move
+		puts "#{@player1.name}, please make your selection. Remember to use the numbers 1-9 for it to be valid"			
+	end
+
+end
+
+#this will be the board that is used during the game
+class Board
+	attr_accessor :board, :game
+
+	def initialize
+		@board = [" ", " ", " ", " ", " " ," ", " ", " "]
+	end
+
 
 	#this presents the board with locations to the player
 	def show_board
 		puts "Below is the board"
 		puts "********************"
-		puts "------|------|------"
-		puts "  1   |  2   |  3   "
-		puts "------|------|------"
-		puts "  4   |  5   |  6   "
-		puts "------|------|------"
-		puts "  7   |  8   |  9   "
-		puts "------|------|------"
+		puts "--------|---------|---------"
+		puts "  1 #{@board[0]}   |  2 #{@board[1]}    |  3 #{@board[2]}   "
+		puts "--------|---------|---------"
+		puts "  4 #{@board[3]}   |  5 #{@board[4]}    |  6 #{@board[5]}   "
+		puts "--------|---------|---------"
+		puts "  7 #{@board[6]}   |  8 #{@board[7]}    |  9 #{@board[8]}   "
+		puts "--------|---------|---------"
 		puts "********************"
 		@board = Board.new
-		@board.instructions
+		board.instructions
 	end
 
-	#this will allow the player to pick a location
-	def make_move
-		puts "From the locations above, please make a selection by number"
-		move = gets.chomp.to_i
-	end
-end
-
-#this will be the board that is used during the game
-class Board
-
-	@board = (0..8).to_a
 
 	def instructions
 		puts "From the graph above, please make a selection of where you would like to move during the game."
+		@game = Game.new
+		game.make_move
 	end
 
 end
